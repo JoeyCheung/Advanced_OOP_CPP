faa3919a229aa49073a4c903bad3ce9992e1109a
Joey Cheung <joeycheung0@gmail.com>
2019-04-03 23:17:14 -0400

    Edited Project2_Cheung_Joey_23145232 file

--- Project2_Cheung_Joey_23145232.cpp
+++ Project2_Cheung_Joey_23145232.cpp
@@ -6,234 +6,124 @@
 #include <stdio.h>
 #include <memory>
 
-
 using namespace std; 
 
 //global variables
-ifstream in_File;
-ofstream outFile;
+ifstream inputFile;
+ofstream outputFile;
 
-class ListNode
-{
+class Node {
 private:
-    friend class linkedList; 
+    friend class PolynomialList; 
 public:
-    int coeff;
-    int expon;
-    string variable;
+    int exponent;
+    int coefficient;
+    string var;
     char op;
-    ListNode* next;
+    Node* n;
     //overload operator = uses l-values
-    ListNode& operator=(const ListNode &rhs) = default;
+    Node& operator=(const Node &rhs) = default;
     //regular constructor
-    ListNode()
-    {
-        this->coeff = 0;
-        this->expon = 0;
-        this->variable = " ";
+    Node() {
+        this->coefficient = 0;
+        this->exponent = 0;
+        this->var = " ";
         this->op = ' ';
-        this->next = nullptr;
-    }
-
-    //Deep copy constructor
-    ListNode(const ListNode &source)
-    {
-        next = new ListNode();
-        next = source.next;
-
-        cout<<"Copy constructor- Deep - data copy: " << next <<endl;
+        this->n = nullptr;
     }
 
     //Move Constructor
-    ListNode(ListNode &&source)
-    {
-        next = new ListNode();
-        next = source.next; 
-        source.next = nullptr;
-    }
+    Node(Node &&source) {
+        n = new Node();
+        n = source.n; 
+        source.n = nullptr;
+    } 
+    
+    //Deep copy constructor
+    Node(const Node &source) {
+        n = new Node();
+        n = source.n;
 
+        cout<<"Copy constructor- Deep - data copy: " << n <<endl;
+    }
 };
 
-class linkedList
-{
+class PolynomialList {
 public:
-    ListNode *front = new ListNode();
+    Node *front = new Node();
 
     //regular constructor
-linkedList()
-{
-    front->next = nullptr;
+PolynomialList() {
+    front->n = nullptr;
 }
 
 //Deep copy constructor
-linkedList(const linkedList &source)
-{
-    front = new ListNode; //allocate storage
+PolynomialList(const PolynomialList &source) {
+    front = new Node; //allocate storage
     *front = *source.front;
 
-    cout << "Copy constructor-Deep- data copy: " << front->next <<endl; 
-}
-/*
-//Move Constructor
-linkedList::linkedList(linkedList &&source):front{source.front}
-{
-    source.front = nullptr;
-}
-//Destructor
-~linkedList()
-{
-    if(front != nullptr)
-    {
-        cout<<"Destructor freeing data for: " << front->str <<endl;
-    }
-    else
-    {
-        cout<<"Destructor freeing data for nullptr" <<endl;
-    }
-
-    delete front; 
-}
-*/
-
-void calculate_Poly()
-{
-    ListNode *curr = this->front;
-    ListNode *prev = this->front; 
-
-	while (curr != nullptr)
-    {
-        prev = curr;
-        curr = curr->next;//so it skips the dummy
-        if (curr->next == nullptr)break; 
-        if (curr->expon == curr->next->expon && curr->variable == curr->next->variable)
-        {
-            ListNode *temp;
-            temp = curr->next;
-            curr->coeff = curr->coeff + curr->next->coeff;
-            prev->next = curr;
-            curr->next = curr->next->next;
-            delete temp;
-        }
-    }
-    output();
+    cout << "Copy constructor-Deep- data copy: " << front->n <<endl; 
 }
 
 //method for calculation
-
-void output()
-{   ListNode *curr;
-    curr = this->front;
-    while(curr->next != nullptr)
-    {
-        outFile <<curr->next->coeff << curr->next->variable << "^" <<
-        curr->next->expon<<endl;
-        curr = curr->next;
+void outputPolynomial() {
+    Node *current;
+    current = this->front;
+    while(current->n != nullptr) {
+        outputFile <<current->n->coefficient << current->n->var << "^" <<
+        current->n->exponent<<endl;
+        current = current->n;
     }
 }
 
-void buildList(int coeff, char variable, char exponent, char op)
-{
-    ListNode *curr = this->front; 
-    ListNode* newNode = new ListNode; 
+void calculate() {
+    Node *current = this->front;
+    Node *prev = this->front; 
 
-    if (coeff == 0)
-    {
-        coeff = 1; 
-    }
-    if(op == '-')
-    {
-        coeff = -(coeff);
-    }
-    if(op == '+')
-    {
-        coeff = +(coeff);
-    }
-
-    newNode->coeff = coeff;
-    newNode->variable = variable;
-    newNode->expon = exponent;
-    newNode->op = op; 
-
-    if(curr->next == nullptr)
-    {
-        curr->next = newNode;
-        newNode->next = nullptr; 
-        return;
-    }
-    else
-    {
-        while(curr->next != nullptr)
-        {
-            if(newNode->expon >= curr->next->expon)
-            {
-                ListNode* temp;
-                temp = curr->next;
-                curr->next = newNode;
-                newNode->next = temp; 
-                break;
-            }
-            else if(curr->next == nullptr)
-            {
-                curr->next = newNode;
-                newNode->next = nullptr; 
-                break;
-            }
-            
-            curr = curr->next;// pointing to the next one
+	while (current != nullptr) {
+        prev = current;
+        current = current->n;//so it skips the dummy
+        if (current->n == nullptr)break; 
+        if (current->exponent == current->n->exponent && current->var == current->n->var) {
+            Node *temp;
+            temp = current->n;
+            current->coefficient = current->coefficient + current->n->coefficient;
+            prev->n = current;
+            current->n = current->n->n;
+            delete temp;
         }
     }
-    return;
+    outputPolynomial();
 }
 
-void loadList(int n)
-{
-    char* arr = new char[n]; 
-    char a;
-
-    for(int i = 0; i < n; i++)
-    {
-        in_File >> a;
-        arr[i] = a;  
-    }
-
-    separate_Polynomial(arr, n);
-}
-
-void separate_Polynomial(char* arr, int n)
-{
+void separate(char* arr, int n) {
     int counter {0};
-    
     char op {' '};
-    int coeff {0};
-    char variable {' '};
-    char exponent {' '};
+    int c {0};
+    char v {' '};
+    char e {' '};
 
-    for(int i = 0; i < n; counter = i)
-    {
+    for(int i = 0; i < n; counter = i) {
          op = ' ';
-         coeff = 0;
-         variable = ' ';
-         exponent = ' ';
+         c = 0;
+         v = ' ';
+         e = ' ';
 
-        if(arr[i] == '+' || arr[i] == '-')
-        {
+        if(arr[i] == '+' || arr[i] == '-') {
             op = arr[i];
             counter++;
         }
 
-        switch(arr[i])
-        {
+        switch(arr[i]) {
             case '+':
-                for(counter; counter < n; counter++)
-                {
-                    switch(arr[counter])
-                    {
+                for(counter; counter < n; counter++) {
+                    switch(arr[counter]) {
                         case '^':
-                            exponent = arr[counter + 1] - '0';
+                            e = arr[counter + 1] - '0';
                             counter++;
                             break;
                         case 'x':
-                            variable = arr[counter];
+                            v = arr[counter];
                             break; 
                         case '+':
                             goto add_node;
@@ -242,47 +132,41 @@ void separate_Polynomial(char* arr, int n)
                             goto add_node;
                             break;
                         default:
-                            if(isdigit(arr[counter]))
-                            {   
+                            if(isdigit(arr[counter])) {   
                                 int result = 0; 
-                                for(counter; arr[counter] != '\0'; counter++)
-                                {
+                                for(counter; arr[counter] != '\0'; counter++) {
                                     if(arr[counter] == '+' || arr[counter] == '-' || arr[counter] == 'x')break; 
                                     result +=  atoi(&arr[counter]);
                                 }
-                                if(arr[counter] == 'x' && arr[counter + 1] == '^')
-                                {
-                                    variable = arr[counter]; 
+                                if(arr[counter] == 'x' && arr[counter + 1] == '^') {
+                                    v = arr[counter]; 
                                 }
-                                else if(arr[counter] == 'x' && arr[counter + 1] != '^')
-                                {
-                                    variable = arr[counter];
-                                    exponent = 1; 
+                                else if(arr[counter] == 'x' && arr[counter + 1] != '^') {
+                                    v = arr[counter];
+                                    e = 1; 
                                 }
-                                else if(arr[counter] != 'x')
-                                {
-                                    variable = 0;
-                                    exponent = 0;
+                                else if(arr[counter] != 'x') {
+                                    v = 0;
+                                    e = 0;
                                 }
 
-                                coeff = result;
-                                if(arr[counter] == '+' || arr[counter] == '-')goto add_node;
+                                c = result;
+                                if(arr[counter] == '+' || arr[counter] == '-')
+                                    goto add_node;
                             }  
                             break;
                     }
                 }
             case '-':
-                for(counter; counter < n; counter++)
-                {
-                    switch(arr[counter])
-                    {   
+                for(counter; counter < n; counter++) {
+                    switch(arr[counter]) {   
                         case '^':
-                            exponent = arr[counter + 1] - '0';
+                            e = arr[counter + 1] - '0';
                             counter++;
                             break;
 
                         case 'x':
-                            variable = arr[counter];
+                            v = arr[counter];
                             break; 
 
                         case '+':
@@ -294,32 +178,28 @@ void separate_Polynomial(char* arr, int n)
                             break;
 
                         default:
-                            if(isdigit(arr[counter]))
-                            {
+                            if(isdigit(arr[counter])) {
                                 int result = 0; 
 
-                                for(counter; arr[counter] != '\0'; counter++)
-                                {
+                                for(counter; arr[counter] != '\0'; counter++) {
                                     if(arr[counter] == '+' || arr[counter] == '-' || arr[counter] == 'x')break;
                                     result += atoi(&arr[counter]); 
                                 }
-                                if(arr[counter] == 'x' && arr[counter + 1] == '^')
-                                {
-                                    variable = arr[counter]; 
+                                if(arr[counter] == 'x' && arr[counter + 1] == '^') {
+                                    v = arr[counter]; 
                                 }
-                                else if(arr[counter] == 'x' && arr[counter + 1] != '^')
-                                {
-                                    variable = arr[counter];
-                                    exponent = 1; 
+                                else if(arr[counter] == 'x' && arr[counter + 1] != '^') {
+                                    v = arr[counter];
+                                    e = 1; 
                                 }
-                                else if(arr[counter] != 'x')
-                                {
-                                    variable = 0;
-                                    exponent = 0;
+                                else if(arr[counter] != 'x') {
+                                    v = 0;
+                                    e = 0;
                                 }
 
-                                coeff = result;
-                                if(arr[counter] == '+' || arr[counter] == '-')goto add_node;
+                                c = result;
+                                if(arr[counter] == '+' || arr[counter] == '-')
+                                    goto add_node;
                             }
                             break; 
                     }
@@ -329,32 +209,85 @@ void separate_Polynomial(char* arr, int n)
                 break;
         }
         add_node:
-        buildList(coeff, variable, exponent, op);
+        build(c, v, e, op);
         i = counter; 
-        cout << "counter: " << counter <<endl; 
     }
         //send list
-        calculate_Poly();
+        calculate();
     }
+void build(int c, char v, char e, char op) {
+    Node *current = this->front; 
+    Node* newNode = new Node; 
+
+    if (c == 0) {
+        c = 1; 
+    }
+    if(op == '-') {
+        c = -(c);
+    }
+    if(op == '+') {
+        c = +(c);
+    }
+
+    newNode->coefficient = c;
+    newNode->var = v;
+    newNode->exponent = e;
+    newNode->op = op; 
+
+    if(current->n == nullptr) {
+        current->n = newNode;
+        newNode->n = nullptr; 
+        return;
+    }
+    else {
+        while(current->n != nullptr) {
+            if(newNode->exponent >= current->n->exponent) {
+                Node* temp;
+                temp = current->n;
+                current->n = newNode;
+                newNode->n = temp; 
+                break;
+            }
+            else if(current->n == nullptr) {
+                current->n = newNode;
+                newNode->n = nullptr; 
+                break;
+            }
+            
+            current = current->n;// pointing to the next one
+        }
+    }
+    return;
+}
+
+void load(int n) {
+    char* arr = new char[n]; 
+    char a;
+
+    for(int i = 0; i < n; i++) {
+        inputFile >> a;
+        arr[i] = a;  
+    }
+
+    separate(arr, n);
+}
+
 };
 
-int main(int argc, char *argv[])
-{
-    linkedList obj;
+int main(int argc, char *argv[]) {
+    PolynomialList obj;
     int n = 32;//length of poly
-    in_File.open(argv[1]);
-    outFile.open(argv[2]);
+    inputFile.open(argv[1]);
+    outputFile.open(argv[2]);
 
-    if(in_File.is_open())
-    {
-        obj.loadList(n);
+    if(inputFile.is_open()) {
+        obj.load(n);
     }
-    else
-    {
+    else {
         cerr<<"Could not open file" <<endl;
         return 1;
     }
     
-    in_File.close(); 
-    outFile.close();
+    inputFile.close(); 
+    outputFile.close();
 }
